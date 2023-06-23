3fc7b66ded7e3cde926f269cc765d8a666421869
amodotomi <amodotomi@gmail.com>
2023-06-23 21:14:33 +0300

    feat: implement openai api

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require github.com/samber/lo v1.38.1
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/cristalhq/aconfig v0.18.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 )
 
 require (
 	github.com/SlyMarbo/rss v1.0.5
+	github.com/cristalhq/aconfig/aconfighcl v0.17.1
 	github.com/defer-panic/news-feed-bot v0.0.0-20230417101226-b549199cfd56
 	golang.org/x/exp v0.0.0-20230131160201-f062dba9d201 // indirect
 )
---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfighcl"
+)
+
+type Config struct {
+	TelegramBotToken     string        `hcl:"telegram_bot_token" env:"TELEGRAM_BOT_TOKEN" required:"true"`
+	TelegramChannelID    int64         `hcl:"telegram_channel_id" env:"TELEGRAM_CHANNEL_ID" required:"true"`
+	DatabaseDSN          string        `hcl:"database_dsn" env:"DATABASE_DSN" default:"postgres://postgres:postgres@localhost:5432/news_feed_bot?sslmode=disable"`
+	FetchInterval        time.Duration `hcl:"fetch_interval" env:"FETCH_INTERVAL" default:"10m"`
+	NotificationInterval time.Duration `hcl:"notification_interval" env:"NOTIFICATION_INTERVAL" default:"1m"`
+	FilterKeywords       []string      `hcl:"filter_keywords" env:"FILTER_KEYWORDS"`
+	OpenAIKey            string        `hcl:"openai_key" env:"OPENAI_KEY"`
+	OpenAIPrompt         string        `hcl:"openai_prompt" env:"OPENAI_PROMPT"`
+}
+
+var (
+	cfg  Config
+	once sync.Once
+)
+
+func Get() Config {
+	once.Do(func() {
+		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+			EnvPrefix: "NFB",
+			Files:     []string{"./config.hcl", "./config.local.hcl", "$HOME/.config/news-feed-bot/config.hcl"},
+			FileDecoders: map[string]aconfig.FileDecoder{
+				".hcl": aconfighcl.New(),
+			},
+		})
+
+		if err := loader.Load(); err != nil {
+			log.Printf("[ERROR] failed to load config: %v", err)
+		}
+	})
+
+	return cfg
+}
